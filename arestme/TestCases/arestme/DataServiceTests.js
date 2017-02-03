0c0ce1e0dae56083823745bdbb83216c0e4c4a7d
shturec <sthrakal@gmail.com>
2017-02-03 09:03:59 +0200

    refactored into finer components

---
+++ arestme/TestCases/arestme/DataServiceTests.js
@@ -0,0 +1,113 @@
+/* globals $ */
+/* eslint-env node, dirigible */
+var orm2 = {
+	dbName: 'TBL_B',
+	properties: [{
+		name: 'id',
+		dbName: 'B_ID',
+		required: true,
+		id: true,
+	},{
+		name: 'text',
+		dbName: 'B_TEXT'
+	},{
+		name: 'baId',
+		dbName: 'BA_ID',
+	}]
+};
+var testDAO2 = require('daoism/dao').get(orm2);
+testDAO2.list = function(){
+	return [{id:1, text:'b'}];
+};
+
+var orm1 = {
+	dbName: 'TBL_A',
+	properties: [{
+		name: 'id',
+		dbName: 'A_ID',
+		required: true,
+		id: true,
+	},{
+		name: 'text',
+		dbName: 'A_TEXT'
+	}],
+	associationSets: {
+		rel1: {
+			dao: function(){return testDAO2;},
+			joinKey: "baId",
+			associationType: "one-to-many",
+		}
+	}
+};
+var testDAO1= require('daoism/dao').get(orm1);
+testDAO1.list = function(){
+	return [{id:1, text:'a'}];
+};
+testDAO1.find = function(id){
+	return {id:1, text:'a'};//this.list().filter(function(a){ return a.id===id;})[0];
+};
+
+require('log/loggers').setLevel(6)
+//as functional mixin
+var daoService = require('arestme/data_service').asService(testDAO1, {}, 'Test DAO Service');
+
+daoService.service();
+//console.info(JSON.stringify(daoService.getResourceHandlersMap(), null, 2));
+
+var assert = require('core/assert');
+try{
+	var members = Object.keys(daoService);
+	assert.assertTrue(members.indexOf("dao")>-1);
+	assert.assertTrue(members.indexOf("logger")>-1);
+	assert.assertTrue(members.indexOf("_oConfiguration")>-1);	
+	assert.assertTrue(members.indexOf("handlers")>-1);	
+	assert.assertTrue(members.indexOf("daoProvider")>-1);		
+	assert.assertTrue(members.indexOf("normalizeMediaTypeHeaderValue")>-1);
+	assert.assertTrue(members.indexOf("service")>-1);	
+} catch(err){
+	console.error(err);
+}
+
+try{
+	assert.assertTrue(Object.keys(daoService.handlers).length===9);
+} catch(err){
+	console.error(err);
+}
+
+try{
+	var handlersMap = daoService.getResourceHandlersMap();
+	assert.assertTrue(Object.keys(handlersMap).length===5);
+} catch(err){
+	console.error(err);
+}
+
+var HandlersProvider = require('arestme/data_service').HandlersProvider;
+var MyProvider = function(){
+	HandlersProvider.call(this);
+};
+MyProvider.prototype = Object.create(HandlersProvider.prototype);
+MyProvider.prototype.getHandlers= function(){
+	return {
+		query: function(){console.info('I am in query');},
+		get: function(){console.info('I am in get');}
+	}
+}
+var provider = new MyProvider();
+var MyService1 = function(dao){
+	require('arestme/data_service').DataService.call(this, provider, 'My service');
+};
+MyService1.prototype = Object.create(require('arestme/data_service').DataService.prototype);
+var svc1 = new MyService1();
+console.info(JSON.stringify(svc1.getResourceHandlersMap(), null, 2))
+svc1.service();
+//daoService.service();
+//as class extensions (todo: move in naother file - at most one service() call per execution
+var MyService = function(dao){
+	require('arestme/data_service').DataService.call(this, dao , 'My service');
+};
+MyService.prototype = Object.create(require('arestme/data_service').DataService.prototype);
+var svc = new MyService(testDAO1);
+
+console.info(JSON.stringify(svc.getResourceHandlersMap(), null, 2));
+
+svc.service();
---
+++ arestme/TestCases/arestme/HttpControllerTests.js
@@ -0,0 +1,25 @@
+/* globals $ */
+/* eslint-env node, dirigible */
+var HttpController = require('arestme/http').HttpController;
+var http = new HttpController()
+.addResourceHandlers({
+	"":{
+		"get": {
+			produces: ['application/json'],
+			handler: function(context, io){
+				io.response.setStatus(io.response.OK);
+				io.response.println('OK');
+			}	
+		}
+	}
+})
+.addResourceHandler("{id}","get", function(ctx, io){
+	io.response.setStatus(io.response.OK);
+	io.response.println(ctx.pathParams.id);
+}).addResourceHandler("{id}/{path}","get", function(ctx, io){
+	io.response.setStatus(io.response.OK);
+	io.response.println(ctx.pathParams.id + '  ' + ctx.pathParams.path);
+}).addResourceHandler("error","get", function(ctx, io){
+	this.sendError(io.response.BAD_REQUEST, 'sample error response');
+});
+http.service();
